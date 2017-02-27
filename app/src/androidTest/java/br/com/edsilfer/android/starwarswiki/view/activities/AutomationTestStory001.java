62b30c799f5cc4a1d6b054c1d2ac062a8553a011
Edgar S Fernandes <fernandes.s.edgar@gmail.com>
2017-02-27 14:40:21 -0300

    Added automation test Story 001

---
+++ app/src/androidTest/java/br/com/edsilfer/android/starwarswiki/view/activities/AutomationTestStory001.java
@@ -0,0 +1,67 @@
+package br.com.edsilfer.android.starwarswiki.view.activities;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.edsilfer.android.starwarswiki.R;
+import br.com.edsilfer.android.starwarswiki.util.Utils;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AutomationTestStory001 {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void automationTestStory001() {
+        // Open FAB Menu
+        onView(withId(R.id.fab_menu)).perform(click());
+
+        // Click search by URL
+        onView(allOf(withClassName(is("com.github.clans.fab.FloatingActionButton")),
+                withParent(withId(R.id.fab_menu)),
+                isDisplayed())).perform(click());
+
+        // Grant permissions
+        Utils.allowPermissionsIfNeeded("android.permission.CAMERA");
+        Utils.allowPermissionsIfNeeded("android.permission.ACCESS_COARSE_LOCATION");
+
+        // Type character URL
+        onView(withId(R.id.input1)).perform(click());
+        onView(withId(R.id.input1)).perform(replaceText("http://swapi.co/api/people/1"), closeSoftKeyboard());
+        onView(withId(R.id.okay)).perform(click());
+
+        // Close FAB Menu
+        onView(withId(R.id.fab_menu)).perform(click());
+
+        // Open character options
+        onView(allOf(withId(R.id.wrapper), withParent(withId(R.id.characters)), isDisplayed())).perform(longClick());
+
+        // Remove character
+        onView(allOf(withId(R.id.md_contentRecyclerView),
+                withParent(withId(R.id.md_contentListViewFrame)),
+                isDisplayed())
+        ).perform(actionOnItemAtPosition(0, click()));
+    }
+
+}
