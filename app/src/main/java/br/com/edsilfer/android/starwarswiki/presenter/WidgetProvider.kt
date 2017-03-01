999c2ccb304a8641fcb7d1c87627c1455451e3b0
Edgar S Fernandes <fernandes.s.edgar@gmail.com>
2017-03-01 15:30:05 -0300

    Added widget to display latested added character

---
+++ app/src/main/java/br/com/edsilfer/android/starwarswiki/presenter/WidgetProvider.kt
@@ -0,0 +1,31 @@
+package br.com.edsilfer.android.starwarswiki.presenter
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import br.com.edsilfer.android.starwarswiki.R
+import com.squareup.picasso.Picasso
+
+
+/**
+ * Created by Edgar Fernandes on 03/01/2017
+ */
+
+class WidgetProvider : AppWidgetProvider() {
+
+    private val TAG = WidgetProvider::class.simpleName
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (id in appWidgetIds) {
+            val rootView = RemoteViews(context.packageName, R.layout.widget_star_wars_wiki)
+            rootView.setTextViewText(R.id.header, "Luke Skywalker")
+            Picasso.with(context)
+                    .load("http://im.ziffdavisinternational.com/ign_br/screenshot/default/lukeskywalkerlightsaber_x3zw.jpg")
+                    .fit()
+                    .centerCrop()
+                    .into(rootView, R.id.thumbnail, intArrayOf(id))
+        }
+    }
+
+}
